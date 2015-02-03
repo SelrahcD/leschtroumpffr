8ef24f539d2bd2e6d46780bbcd52e6ed95697b58
SelrahcD <charles@evaneos.com>
2015-02-03 09:49:22 +0100

    Do not tweet if no schtrou

--- app.js
+++ app.js
@@ -14,8 +14,14 @@ twitterClient.stream('statuses/filter', {follow: [24744541]}).on('tweet', functi
 	    	
 		    	// tweet
 		    	console.log(text);
-		    	twitterClient.post('statuses/update', { status: text }, function(err, data, response) {
-				});
+		    	if(text.indexOf('schtrou') > -1) {
+		    		console.log('Twitted');
+		    		twitterClient.post('statuses/update', { status: text }, function(err, data, response) {
+						});
+		    	}
+		    	else {
+		    		console.log('No schtrou');
+		    	}
 		    },
 		    function(a) {
 		    	console.log(a);
